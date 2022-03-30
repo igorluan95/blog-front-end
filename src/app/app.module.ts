95cc9ebcba0f6053a6665593cbb632c809cfc668
Igorluan95 <igorluansouzasilva@gmail.com>
2022-03-30 18:07:43 -0300

    Atualização das importações e declarações de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common'
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -28,7 +29,10 @@ import { InicioComponent } from './inicio/inicio.component';
     HttpClientModule,
     FormsModule        // dependencia
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
