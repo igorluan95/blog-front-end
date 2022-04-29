1b3a61fe40cd211c392a8278d71848faae71ebf2
Igorluan95 <igorluansouzasilva@gmail.com>
2022-04-28 23:48:56 -0300

    Mudança na interface e nos alertas do formulário

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,10 +1,10 @@
+import { invalid } from '@angular/compiler/src/render3/view/util';
 import { AuthService } from './../service/auth.service';
 import { User } from './../model/User';
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertasService } from '../service/alertas.service';
 import { environment } from 'src/environments/environment.prod';
-
 
 @Component({
   selector: 'app-cadastrar',
@@ -18,104 +18,83 @@ export class CadastrarComponent implements OnInit {
   tipoUsuario: string
   fotoUsuario: string
   senhaAdm: string
-  nome: String
-  okAdm = false
+  respSenha = false
 
   constructor(
     private authService: AuthService,
     private router: Router,
     private alertas: AlertasService
 
-     ) { }
+  ) { }
 
-    ngOnInit(){
-      if(environment.token != ''){
-        this.router.navigate(['/inicio'])
-      }
-       window.scroll(0,0)
-
+  ngOnInit() {
+    if (environment.token != '') {
+      this.router.navigate(['/inicio'])
     }
+    window.scroll(0, 0)
 
-    confirmSenha(event: any) {
-      this.confirmarSenha = event.target.value
-      }
-
-    tipoUser(event: any) {
-        this.tipoUsuario = event.target.value
-      }
-
-
-  validarFoto(event: any) {
-    this.fotoUsuario = event.target.value
   }
 
-  validarNome(event: any) {
-    this.nome = event.target.value
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
   }
 
-  validarUsuario(event: any) {
-    this.user = event.target.value
+  tipoUser(event: any) {
+    this.tipoUsuario = event.target.value
   }
-  receberSenhaAdm(event:any) {
+
+  receberSenhaAdm(event: any) {
     this.senhaAdm = event.target.value
   }
 
   validarSenhaAdm() {
-    if(this.senhaAdm == "1234") {
-      this.okAdm = true
+    if (this.senhaAdm == "1234") {
+      this.respSenha = true
       this.alertas.showAlertSuccess("Código correto, continue com o cadastro de Administrador")
     }
     else {
-      this.alertas.showAlertDanger("Código incorreto, tente novamente")
-      this.okAdm = false
+      this.alertas.showAlertDanger("Acesso negado, tente novamente ou mude para tipo Normal")
+      this.respSenha = false
     }
-    return this.okAdm
+    return this.respSenha
   }
 
+  cadastrar() {
 
-      cadastrar() {
+    if (this.tipoUsuario != "Normal" && this.tipoUsuario != "Administrador") {
+      this.alertas.showAlertDanger('Selecione o tipo de perfil!')
+    }
+    else if (this.tipoUsuario == "Administrador" && this.respSenha == false) {
+      this.alertas.showAlertDanger('Insira e confirme o código de Administrador!')
+    }
+    else if (this.user.senha != this.confirmarSenha) {
+      this.alertas.showAlertDanger('As senhas estão divergentes!')
+    }
 
-        if (this.tipoUsuario != "Normal" && this.tipoUsuario != "Administrador") {
-          this.alertas.showAlertDanger('Selecione o tipo de perfil!')
-        }
-        else if (this.tipoUsuario == "Administrador" && this.okAdm == false ) {
-          this.alertas.showAlertDanger('Insira o código de Administrador!')
-        }
-        else if (this.user.nome == '' ) {
-          this.alertas.showAlertDanger('Insira um nome válido!')
-        }
-        else if (this.user.usuario == '') {
-          this.alertas.showAlertDanger('Insira um e-mail válido!')
-        }
-        else if (this.user.senha != this.confirmarSenha) {
-          this.alertas.showAlertDanger('As senhas estão incorretas!')
-        }
+    else {
 
-        else {
+      if (this.fotoUsuario == null || this.fotoUsuario == undefined) {
 
-          if (this.fotoUsuario == null) {
+        this.fotoUsuario = "https://i.imgur.com/k4Iuelh.jpg"
+      }
+      this.user.tipo = this.tipoUsuario
+      this.user.foto = this.fotoUsuario
+      this.authService.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp
+        this.alertas.showAlertSuccess('Usuário cadastrado com sucesso!')
+        this.router.navigate(['/entrar'])
+      },
 
-            this.fotoUsuario = "https://i.imgur.com/k4Iuelh.jpg"
+        erro => {
+          if (erro.status == 500 || erro.status == 401 || erro.status == 400) {
+            this.alertas.showAlertDanger('Campos preenchidos incorretamente ou e-mail já cadastrado')
           }
 
-
-          this.user.tipo = this.tipoUsuario
-          this.user.foto = this.fotoUsuario
-          this.authService.cadastrar(this.user).subscribe((resp: User) => {
-            this.user = resp
-            this.alertas.showAlertSuccess('Usuário cadastrado com sucesso!')
-            this.router.navigate(['/entrar'])
-          },
-
-            erro => {
-              if (erro.status == 500 || erro.status == 401 || erro.status == 400) {
-                this.alertas.showAlertDanger('Campos incorretos ou usuário já cadastrado!')
-              }
-            }
-
-          )
         }
 
-      }
-
+      )
     }
+
+  }
+
+}
